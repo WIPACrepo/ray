27ab7d3ee694f501476de5095ef7939e0e0b9ab3
Alec <asheperd@icecube.wisc.edu>
2026-04-16 14:58:26 -0500

    few path changes for batch translate script

--- ray-translate/translate_chapters.sh
+++ ray-translate/translate_chapters.sh
@@ -5,9 +5,9 @@ URL="https://raytest.icecube.aq/predict"
 CHUNK_SIZE=5
 TOKEN_SIZE=1000
 
-TIMESTAMP=`date +%s`
+TIMESTAMP=$(date +%s)
 
 for i in $(seq 1 $CHAPTERS); do
-    CHAPTER=$(printf "%03d\n" ${i})
-    ./client.py --url "$URL" --chunk_size $CHUNK_SIZE --token_size $TOKEN_SIZE chapters/chapter_$CHAPTER.txt >> translation_$TIMESTAMP &
+  CHAPTER=$(printf "%03d\n" ${i})
+  ./client.py --url "$URL" --chunk_size $CHUNK_SIZE --token_size $TOKEN_SIZE mobydick/chapters/chapter_$CHAPTER.txt >>translation_$TIMESTAMP &
 done
